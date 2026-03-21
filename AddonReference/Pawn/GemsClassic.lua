b89923d86fc5d78e29bde4f1aaa1048458ffb355
srbennett3 <srbennett3@users.noreply.github.com>
2026-03-21 16:16:38 -0700

    feat: add Druid specialization files for Balance, Bear, Cat, and Restoration
    - Introduced new Lua files for Druid specializations: DruidBalance.lua, DruidBear.lua, DruidCat.lua, and DruidRestoration.lua.
    - Each file registers specifications and includes relevant gems, enchants, and items for their respective roles.
    - Enhanced item handling for Druid classes, improving the overall functionality of the BIS planner.

---
+++ AddonReference/Pawn/GemsClassic.lua
@@ -0,0 +1,14 @@
+-- Pawn by Vger-Azjol-Nerub
+-- www.vgermods.com
+-- © 2006-2026 Travis Spomer.  This mod is released under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 license.
+-- See Readme.md for more information.
+--
+-- Gem information (Classic Era)
+------------------------------------------------------------
+
+
+PawnGemQualityLevels = nil
+PawnMetaGemQualityLevels = nil
+PawnCogwheelQualityLevels = nil
+PawnCrystalOfFearQualityLevels = nil
+PawnMinimumItemLevelToConsiderGems = 10000
